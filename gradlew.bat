80f9f88321a32d080c9c7e6c5d0253c61b08f0e0
SofyayaZ <soniazaitceva@gmail.com>
2024-03-02 06:33:58 +0300

    3 commits branch1

--- gradlew.bat
+++ gradlew.bat
@@ -49,7 +49,6 @@ echo. 1>&2
 echo Please set the JAVA_HOME variable in your environment to match the 1>&2
 echo location of your Java installation. 1>&2
 
-goto fail
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
